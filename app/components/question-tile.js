dcc382b276352bb65ad90287b3d5d1fe15f105e9
Nathaniel I Meyer <nathanielimeyer@gmail.com>
2017-04-28 13:59:41 -0700

    Add update and new question actions

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isImageShowing: false,
+  updateQuestionForm: false,
+  actions: {
+    update(question, params) {
+      this.sendAction('update', question, params);
+    }
+    //,
+    //  delete(question) {
+    //   if (confirm('Are you sure you want to delete this question?')) {
+    //     this.sendAction('destroyQuestion', question);
+    //   }
+    // }
+  }
 });
